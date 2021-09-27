08b941e3395435ebf8ca36323acde66518ae2c7e
Clint Ngo <clintngo@gmail.com>
2021-09-27 14:02:06 -0500

    commit

---
+++ fundamentals/introduction/For_loops_basics.py
@@ -0,0 +1,25 @@
+for i in range (0,151) :
+    print(i)
+
+for i in range (5,1001,5):
+    print(i)
+
+for i in range (1,101):
+    if i%10==0:
+        print("Coding Dojo")
+    elif i%5==0:
+        print("Coding")
+    else:
+        print(i)
+
+sum = 0
+for i in range(1,500001):
+    if i%2 != 0:
+        sum= sum +i
+        print(sum)
+
+for i in range(2018,0,-4):
+    print(i)
+
+for i in range (5, 2020, 20):
+    print (i)
