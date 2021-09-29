68caa535051031faf4025e2d3743b4c2c11e8b0c
Clint Ngo <clintngo@gmail.com>
2021-09-29 18:04:45 -0500

    commit

---
+++ fundamentals/oop/Dojopets/run.py
@@ -0,0 +1,7 @@
+from Child.Ninja import Ninja
+from Child.Pets import Pet
+
+nikey = Dog("Nikey", "dog", "spin")
+clint = Ninja("Clint", "Ngo", "jerkey", 2)
+
+clint.walk(nikey).feed(nikey).bathe(nikey)
